e0ed0d25479b821e4083ac23a40372d88951f124
Priyanshu1234-kumar <81919105+Priyanshu1234-kumar@users.noreply.github.com>
2023-07-02 18:48:04 +0530

    Update ProjectData.js

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,6 +1,15 @@
 export const Project = [
     {
         id:1,
+        name:"AugiTrons",
+        description:"A marker-based augmented reality (AR) enabled online electronics purchasing web application is presented to provide users with an intuitive and interactive shopping experience.",
+        tags:["html","javascript","styledComponents","Python","Django","Bootstrap","css"],
+        
+        demo:"https://www.linkedin.com/in/priyanshu-kumar-b9ba611ba/details/projects/",
+        github:"https://github.com/Pk686/website.github.io"
+    },
+    {
+        id:2,
         name:"  Ecommerce",
         description:"A website that allows people to buy physical goods, digital products over the internet rather than at a brick-and-mortar location.",
         tags:["html","javascript","styledComponents","Python","Django","Bootstrap","css"],
@@ -9,7 +18,7 @@ export const Project = [
         github:"https://github.com/Priyanshu1234-kumar/ecommerce"
     },
     {
-        id:2,
+        id:3,
         name:"Movieapp",
         description:"A web application that offers overviews of movies encouraging users to read it in details.",
         tags:["html","sass","styledComponents","javascript","Bootstrap","css","api"],
@@ -19,7 +28,7 @@ export const Project = [
         
     },
     {
-        id:3,
+        id:4,
         name:"Echo-health",
         description:"A web application that helps manage the information related to health care and manage the data related to all Departments of healthcare such as clinical, Financial laboratory.",
         tags:["html","javascript","Python","Django","css"],
@@ -28,7 +37,7 @@ export const Project = [
         github:"https://github.com/Priyanshu1234-kumar/Echo-health"
     },
     {
-        id:4,
+        id:5,
         name:"3d-car website",
         description:"A cool looking 3-dimension car website. It has really smooth animations.",
         tags:["html","javascript","css"],
@@ -36,8 +45,9 @@ export const Project = [
         demo:"https://github.com/Priyanshu1234-kumar/carwebsite3d",
         github:"https://github.com/Priyanshu1234-kumar/carwebsite3d"
        
-    },{
-        id:5,
+    },
+    {
+        id:6,
         name:"AnonyVit",
         description:"A Blogging website founded by three fellow mates to post some unique information that engineering students must know.",
         tags:["html","Bootstrap","css"],
